88ebad4a614f0a7a49cbb8934ec24eed5046b7c3
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-08 14:57:46 -0400

    Testing

--- layouts/Theme.tsx
+++ layouts/Theme.tsx
@@ -2,5 +2,5 @@ import { ThemeProvider } from "styled-components";
 import { light, dark, midnight } from "../utils/theme";
 
 export const Theme = ({ children }: any): JSX.Element => {
-  return <ThemeProvider theme={dark}>{children}</ThemeProvider>;
+  return <ThemeProvider theme={midnight}>{children}</ThemeProvider>;
 };
