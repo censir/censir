912e31afe958923533add3948e70ecbee405227e
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-08 00:02:23 -0400

    Feat: Theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { dark } from "../utils/theme";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={dark}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 export default MyApp;
