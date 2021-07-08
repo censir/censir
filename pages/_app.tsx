82c581b5fa13e67fbaec9354fce362c27765fafc
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-08 13:24:41 -0400

    Feat: Global theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from "next/app";
 import { Theme } from "../layouts/Theme";
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  body {
+    font-family: "Poppins", "sans-serif";
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Theme>
+      <Global />
       <Component {...pageProps} />
     </Theme>
   );
