e5f312c8e148d87b646e96ccf84a0f62b2138d80
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-08 13:24:57 -0400

    Feat: Semi working theming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import { Logo } from "../components";
+import styled from "styled-components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Logo />
+      <SomeComp>test</SomeComp>
     </div>
   );
 }
+
+const SomeComp = styled.h1`
+  colors: ${(props) => props.theme.colors.primary};
+`;
