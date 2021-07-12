25e002220aca36518d3b5fcb28cc14e7accf12a9
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-12 16:55:21 -0400

    Refactor: Moved

---
+++ pages/api/weather/pill/[lat]/[lon].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Weather } from "../../../../../utils/Weather";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const lat = Number(req.query.lat);
+  const lon = Number(req.query.lon);
+
+  const currentWeather = await Weather.getCurrentWeatherByGeoCoordinates(
+    lat,
+    lon
+  );
+
+  console.log(currentWeather);
+
+  if (Number(currentWeather.cod) === 400)
+    return res.status(400).json({
+      success: false,
+      message: "Bad latitude or longitude!",
+    });
+
+  res.json(currentWeather);
+}
