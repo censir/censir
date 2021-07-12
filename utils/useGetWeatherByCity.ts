4fea3cbc3316e7b1baf5a06b241c6367caecddc5
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-12 14:47:43 -0400

    Fix: Use country in use SWR

--- utils/useGetWeatherByCity.ts
+++ utils/useGetWeatherByCity.ts
@@ -2,8 +2,8 @@ import useSWR from "swr";
 import { fetcher } from "./fetcher";
 import { Weather } from "../types/types";
 
-export const useGetWeatherByCity = (city: string) => {
-  const { data, error } = useSWR(`/api/weather/${city}`, fetcher);
+export const useGetWeatherByCity = (city: string, country: string) => {
+  const { data, error } = useSWR(`/api/weather/${city}/${country}`, fetcher);
 
   return {
     weather: data as Weather,
