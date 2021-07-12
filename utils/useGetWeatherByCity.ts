8caf07a4a0e30d8d22e4b911db5a1ef7e79fc1eb
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-12 14:06:24 -0400

    Feat: better typings + fetcher that throws errors

--- utils/useGetWeatherByCity.ts
+++ utils/useGetWeatherByCity.ts
@@ -1,11 +1,12 @@
 import useSWR from "swr";
-import { CurrentResponse } from "openweathermap-ts/dist/types/index";
+import { fetcher } from "./fetcher";
+import { Weather } from "../types/types";
 
 export const useGetWeatherByCity = (city: string) => {
-  const { data, error } = useSWR(`/api/weather/${city}`);
+  const { data, error } = useSWR(`/api/weather/${city}`, fetcher);
 
   return {
-    weather: data as CurrentResponse,
+    weather: data as Weather,
     isLoading: !error && !data,
     isError: error,
   };
