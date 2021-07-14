f73221cec35290625484d32ca1a347f4143f63f9
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-14 01:11:47 -0400

    Refactor: Better paths + no reloading

--- components/pill.tsx
+++ components/pill.tsx
@@ -1,21 +1,18 @@
-// @ts-expect-error Types :(
-import { useIpCoords } from "use-ipcoords";
-import { useGetPill } from "../utils/useGetPill";
+import { GetCoords } from "@/utils/getCoords";
+import { useGetPill } from "@/utils/useGetPill";
 
 export const Pill = (): JSX.Element => {
-  const { latitudeIp: lat, longitudeIp: lon } = useIpCoords();
-
+  const { latitude: lat, longitude: lon } = GetCoords();
   const { weather, isLoading, isError } = useGetPill(lat, lon);
 
   if (isLoading) console.log("loading");
-  if (isError) console.log("loading");
 
   return (
     <div>
       {isError ? (
         <h1>An error occurred!</h1>
       ) : !isLoading && weather ? (
-        <span>{weather.main.temp}</span>
+        <span>{weather.cod}</span>
       ) : (
         <span>Loading...</span>
       )}
