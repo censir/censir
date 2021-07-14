fbe925883750174a47587bcb77a54f406bfc3c65
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-14 17:58:12 -0400

    Feat: Check if you have both city and country

--- utils/useGetWeatherByCity.ts
+++ utils/useGetWeatherByCity.ts
@@ -3,7 +3,10 @@ import { fetcher } from "./fetcher";
 import { Weather } from "@/types/types";
 
 export const useGetWeatherByCity = (city: string, country: string) => {
-  const { data, error } = useSWR(`/api/weather/${city}/${country}`, fetcher);
+  const { data, error } = useSWR(
+    city && country ? `/api/weather/${city}/${country}` : null,
+    fetcher
+  );
 
   return {
     weather: data as Weather,
