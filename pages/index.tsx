a5a2b014f293a3093f11dcf992e66cffd2efdb16
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-13 22:47:03 -0400

    Some testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,29 @@ import styled from "styled-components";
 
 // @ts-expect-error Types :(
 import { useIpCoords } from "use-ipcoords";
+import { useGetPill } from "../utils/useGetPill";
 
 export default function Home(): JSX.Element {
-  const { latitudeIp, longitudeIp } = useIpCoords();
-  console.log(latitudeIp, longitudeIp);
+  const { latitudeIp: lat, longitudeIp: lon } = useIpCoords();
+
+  const { weather, isLoading, isError } = useGetPill(lat, lon);
+
+  if (isLoading) console.log("loading");
+  if (isError) console.log("loading");
+
+  console.log(weather);
   return (
     <div>
       <LeftContainer>
         <Logo />
         <InputContainer>
+          {isError ? (
+            <h1>An error occurred!</h1>
+          ) : !isLoading && weather ? (
+            <span>{weather.cod}</span>
+          ) : (
+            <span>Loading...</span>
+          )}
           <Title>Check the weather</Title>
           <Inputs />
         </InputContainer>
