d02280a3080da63deb2ff1a38b33f52b881d4ce5
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-14 01:11:59 -0400

    Refactoring: better paths

--- layouts/Theme.tsx
+++ layouts/Theme.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components";
-import { light, dark, midnight } from "../utils/theme";
+import { light, dark, midnight } from "@/utils/theme";
 
 export const Theme = ({ children }: any): JSX.Element => {
   return <ThemeProvider theme={midnight}>{children}</ThemeProvider>;
