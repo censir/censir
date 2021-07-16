d9cda54785e4e93ee012cb93b94a88466c54d1ce
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-15 23:27:34 -0400

    Feat: Modify inputs a little

--- components/inputs.tsx
+++ components/inputs.tsx
@@ -13,16 +13,17 @@ export const Inputs = (): JSX.Element => {
 const Input = styled.input`
   display: block;
   margin-left: 20px;
-  border-radius: 10px;
+  border-radius: 25px;
   margin-top: 20px;
-  min-width: 230px;
+  min-width: 340px;
   max-width: 300px;
   width: 80%;
-  padding: 13px 15px;
+  padding: 13px 0px;
   font-size: ${(props) => props.theme.fontSizes.small};
   border: none;
   background: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.textColor};
+  text-align: center;
   filter: drop-shadow(0px 4px 17px rgba(0, 0, 0, 0.23));
 
   &:first-child {
@@ -31,6 +32,10 @@ const Input = styled.input`
 
   &::placeholder {
     opacity: 0.5;
+  }
+
+  &:focus {
+    outline: 0;
   }
 `;
 
