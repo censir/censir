cfe4e6020e85cc86fccd7e857d2086f93a86db93
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-17 14:07:17 -0400

    Fix: Bunch of stuff

--- components/coolButton.tsx
+++ components/coolButton.tsx
@@ -1,18 +1,47 @@
 import styled from "styled-components";
+import { ArrowRight } from "react-feather";
 
 export const Button = ({ text }: any): JSX.Element => {
-  console.log(text);
-  return <StyledButton>{text}</StyledButton>;
+  return (
+    <StyledButton>
+      <I>{text}</I> <Arrow size={22} />
+    </StyledButton>
+  );
 };
 
+const Arrow = styled(ArrowRight)`
+  &&& {
+    vertical-align: middle;
+    color: ${(props) => props.theme.colors.accent};
+    transition: ${(props) => props.theme.transitions.easeInOut};
+    will-change: transform;
+  }
+`;
+
 const StyledButton = styled.button`
-  border-radius: 10px;
-  padding: 10px 12px;
-  pointer: cursor;
+  border-radius: 13px;
+  padding: 6px 12px;
+  cursor: pointer;
   border: none;
-  font-size: 1em;
-  font-weight: bold;
+  font-size: .95em;
+  font-family: "Poppins", sans-serif;
   color: ${(props) => props.theme.colors.darkTextColor};
   background: ${(props) => props.theme.colors.secondary};
-  box-shadow: ${(props) => props.theme.shadows.normalShadow};
+  box-shadow: ${(props) => props.theme.shadows.subtleShadow};
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.shadows.normalShadow};
+  }
+
+  &:hover ${Arrow} {
+    transform: translateX(3px);
+  }
+`;
+
+const I = styled.span`
+  vertical-align: middle;
+  font-style: unset;
+  font-weight: 600;
+  margin-right: 8px;
+  margin-left: 2px;
 `;
