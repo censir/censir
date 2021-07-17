3f7320da8a390254e5b88867da0211a7d2ca7872
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-17 12:35:42 -0400

    Feat: Set default font weight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { SkeletonTheme } from "react-loading-skeleton";
 const Global = createGlobalStyle`
   body {
     font-family: Poppins, sans-serif;
+    font-weight: normal;
     background: ${(props: any) =>
       props.theme.colors.primary} url(./img/cloud.svg) no-repeat top left;
   }
