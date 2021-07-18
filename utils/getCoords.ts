eddd7276bb3f6650fecf8cd34b6a2d1b34170b76
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-18 18:53:03 -0400

    Feat: Geolocation support!

--- utils/getCoords.ts
+++ utils/getCoords.ts
@@ -6,12 +6,30 @@ export const GetCoords = (): GetCoordsRes => {
   const [longitude, setLongitude] = useState<number | null>(null);
 
   useEffect(() => {
-    fetch("https://get.geojs.io/v1/ip/geo.json")
-      .then((res) => res.json())
-      .then((location) => {
-        setLatitude(location.latitude);
-        setLongitude(location.longitude);
-      });
+    const getLocationViaIp = () => {
+      fetch("https://get.geojs.io/v1/ip/geo.json")
+        .then((res) => res.json())
+        .then((location) => {
+          setLatitude(location.latitude);
+          setLongitude(location.longitude);
+        });
+    };
+    navigator.permissions.query({ name: "geolocation" }).then(({ state }) => {
+      if (state === "denied") return getLocationViaIp();
+
+      // If prompted, don't return because we want to ask for permission
+      if (state === "prompt") getLocationViaIp();
+
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setLatitude(pos.coords.latitude);
+          setLongitude(pos.coords.longitude);
+        },
+
+        // if they deny or another error occurs, get location via IP
+        () => getLocationViaIp()
+      );
+    });
   }, []);
 
   return { latitude, longitude };
