6de6cde168fd4aa89464760a87121aee0ec127a7
looskie <50378828+Looskie@users.noreply.github.com>
2021-07-19 16:43:07 -0400

    Feat: Go to home with click

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import styled from "styled-components";
 
 export const Logo = (): JSX.Element => {
-  return <Image src='/img/logo.svg' alt='CENSIR logo' draggable={false} />;
+  return (
+    <Link passHref={true} href='/'>
+      <Image src='/img/logo.svg' alt='CENSIR logo' draggable={false} />
+    </Link>
+  );
 };
 
 const Image = styled.img`
   margin: 20px 20px 0;
   width: 125px;
   height: auto;
+  cursor: pointer;
 `;
